99365a37cac5ccd6af98ead245c82e98be67cc91
Szymon Wieczorek <doszymona@gmail.com>
2024-03-30 21:34:00 +0100

    backend and frontend together by SpringBoot

--- build/run.sh
+++ build/run.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-echo "This script should start your application"
-
 docker rm -f $(docker ps -a -q)
-docker image rm b
-docker image rm tictactoebackend:v1 tictactoebackend:latest
-docker image rm tictactoefrontend:v1 tictactoefrontend:latest
+docker image rm tictactoe:v1 tictactoe:latest
 
 cd ..
-cd backend/
+cd app/
 
-docker build -t tictactoebackend:v1 -t tictactoebackend:latest .
-
-cd ..
-cd frontend/
-
-docker build -t tictactoefrontend:v1 -t tictactoefrontend:latest .
+docker build -t tictactoe:v1 -t tictactoe:latest .
 
 cd ..
 docker compose up
