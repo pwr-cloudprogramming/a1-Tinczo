453ab8d540b45bff282332afb3c1880f94793ff5
Szymon Wieczorek ec2 <doszymona@gmail.com>
2024-04-15 08:20:16 +0000

    dynamic ip address definition

--- frontend/src/js/socket_js.js
+++ frontend/src/js/socket_js.js
@@ -1,4 +1,4 @@
-const url = 'http://ec2-54-160-91-206.compute-1.amazonaws.com:8080';
+// const url = 'http://ec2-54-160-91-206.compute-1.amazonaws.com:8080';
 let stompClient;
 let gameId;
 let playerType;
@@ -107,3 +107,5 @@ function connectToSpecificGame() {
         })
     }
 }
+
+
